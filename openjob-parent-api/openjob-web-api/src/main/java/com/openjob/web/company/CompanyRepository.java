43137cba96c5d1378aa24b2f0f4d4ade41fb647d
luuAG <duongvannam2001@gmail.com>
2023-07-11 22:52:39 +0700

    heyzo

--- openjob-parent-api/openjob-web-api/src/main/java/com/openjob/web/company/CompanyRepository.java
+++ openjob-parent-api/openjob-web-api/src/main/java/com/openjob/web/company/CompanyRepository.java
@@ -14,19 +14,23 @@ import java.util.Optional;
 public interface CompanyRepository extends JpaRepository<Company, String> {
     Optional<Company> findById(String id);
 
-    @Query("select c from Company c where c.address like %?1%")
+    @Query("select c from Company c where c.address like %?1% and c.isActive=true")
     Page<Company> findByLocation(String location, Pageable pageable);
 
-    @Query("select c from Company c where c.name like %?1% or c.description like %?1%")
+    @Query("select c from Company c where c.name like %?1% or c.description like %?1% and c.isActive=true")
     Page<Company> findByKeyword(String keyword, Pageable pageable);
 
-    @Query("select c from Company c where (c.name like %?1% or c.description like %?1%) and c.address like %?2%")
+    @Query("select c from Company c where (c.name like %?1% or c.description like %?1%) and c.address like %?2% and c.isActive=true")
     Page<Company> findByKeywordAndLocation(String keyword, String location, Pageable pageable);
 
     @Query("update Company c set c.accountBalance = c.accountBalance + ?2 where c.id=?1")
     @Modifying
     void updateAccountBalance(String companyId, Double amount);
 
-    @Query("select u.company from User u where u.id = ?1")
+    @Query("select u.company from User u where u.id = ?1 and u.company.isActive=true")
     Company findByHeadHunterId(String id);
+
+
+    @Query("select c from Company c where c.isActive=true")
+    Page<Company> findAll(Pageable pageable);
 }
